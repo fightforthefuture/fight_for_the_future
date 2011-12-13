82919fe34ebba4c83557b7340b2815b93c64cfce
Tyler Hunt <tyler@tylerhunt.com>
2011-12-13 10:35:22 -0800

    Implement initial version of image censor.

--- censorship.gemspec
+++ censorship.gemspec
@@ -13,6 +13,5 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ['lib']
 
-  # s.add_development_dependency "rspec"
-  # s.add_runtime_dependency "rest-client"
+  s.add_runtime_dependency 'rmagick', '~> 2.13.1'
 end
--- lib/censorship.rb
+++ lib/censorship.rb
@@ -1,4 +1,59 @@
 require 'censorship/version'
+require 'RMagick'
 
 module Censorship
+  class Image
+    def initialize(original_path)
+      @original_path = original_path
+    end
+
+    def write(censored_path)
+      image = Magick::ImageList.new(@original_path)
+      image = image.gaussian_blur(0, 3)
+
+      overlay(image)
+      annotate(image, 'THIS IMAGE CENSORED', 10)
+      annotate(image, 'http://censr.net/23985', 20, Magick::SouthGravity)
+
+      image.write(censored_path)
+    end
+
+    def overlay(image, color='#000000CC')
+      overlay = Magick::Image.new(image.columns, image.rows) do
+        self.background_color = color
+      end
+
+      image.composite!(
+        overlay,
+        Magick::ForgetGravity,
+        Magick::MultiplyCompositeOp
+      )
+    end
+    private :overlay
+
+    def annotate(image, text, margin, gravity=Magick::CenterGravity)
+      canvas = Magick::Image.new(1500, 200) do
+        self.background_color = '#00000000'
+      end
+
+      annotation = Magick::Draw.new
+
+      annotation.annotate(canvas, 0, 0, 0, 0, text) do
+        self.fill = 'white'
+        self.stroke = 'none'
+        self.gravity = Magick::CenterGravity
+        self.pointsize = 80
+        self.font_family = 'Helvetica'
+        self.font_weight = Magick::BoldWeight
+      end
+
+      max_width = image.columns - image.columns / (100 / margin)
+      max_height = image.rows / 4
+
+      canvas.trim!(true)
+      canvas.resize_to_fit!(max_width, max_height)
+      image.composite!(canvas, gravity, Magick::OverCompositeOp)
+    end
+    private :annotate
+  end
 end
